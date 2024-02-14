b07beebf8b4964e7742adb921acd83bc6b614cd3
Harikrishnan PS <harikrishnan.ps@hotmail.com>
2024-02-15 01:53:51 +0530

    Update mapscript.js

--- mapscript.js
+++ mapscript.js
@@ -1281,7 +1281,7 @@ paths.forEach(path => {
     if (services && services.length > 0) {
         path.style.fill = "#F07AA1";
         path.addEventListener('mouseenter', function() {
-            this.style.fill = 'linear-gradient(#FF347D, #827DED)';
+            this.style.fill = "#827DED";
         });
         path.addEventListener('mouseleave', function() {
             this.style.fill = "#F07AA1";
